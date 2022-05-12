7efcb48b990f9745c113032f792cd004850ea450
Giordano <giordano_lipari@yahoo.co.uk>
2022-05-12 14:22:44 +0200

    Add comment

--- middle.sh
+++ middle.sh
@@ -1,3 +1,7 @@
+# input: a list of text files
+# action: take the lines between number 10 and 15
+# output: show the results on screen
+
 for filename in "$@" do
 head -n 15 $filename | tail -5
 done
