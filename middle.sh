fbab4ffe101b5dd1682a0f677cab75f5a2a19772
Giordano <giordano_lipari@yahoo.co.uk>
2022-05-12 15:34:33 +0200

    Add something stupid; Fix for-loop error

--- middle.sh
+++ middle.sh
@@ -2,9 +2,11 @@
 # action: take the lines between number 10 and 15
 # output: show the results on screen
 
-for filename in "$@" do
+for filename in "$@" 
+do
 echo $filename
 head -n 15 $filename | tail -5
+something stupid
 echo
 done
 
