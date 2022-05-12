649b4c11eec21714c9c53c332e4105ba24e7242d
schranzhofer <hermann.schranzhofer@tugraz.at>
2022-05-12 16:54:16 +0200

    did some changes

--- middle.sh
+++ middle.sh
@@ -1,13 +1,13 @@
 # input: a list of text files
 # action: take the lines between number 10 and 15
 # output: show the results on screen
+# final remark on top
 
 for filename in "$@" 
 do
 echo $filename
 head -n 15 $filename | tail -5
-something stupid
+something very stupid
 echo
 done
 
-# a final remark
